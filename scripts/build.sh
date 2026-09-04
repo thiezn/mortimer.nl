b1bc34ce2448c17c8156bbc2ddfe5f1f822502a9
Mathijs <thiezn@gmail.com>
2026-09-04 17:49:11 +0200

    rebuilt site with updated version of mdsite

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 echo "Installing my vibe coded Static Site Generator..."
 cargo install --git https://github.com/thiezn/mdsite
+# Do force if I haven't bumped the crate version again :D
+# cargo install --git https://github.com/thiezn/mdsite --force
 
 echo "Building the site. Github pages supports the docs/ folder out of the box. Make sure to commit to main after running this script."
 mdsite build --input website --output docs/
